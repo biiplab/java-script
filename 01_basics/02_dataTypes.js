66a00ea3d17a080e1438c2e7e1b5f76e16b7a972
biiplab <biiplabroyy@gmail.com>
2024-09-06 13:38:30 +0530

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";  // treat all JS code as newer version
+
+// alert("hello") // we are using node js, not browser.. so it will show error
+
+console.log( 3+3); 
+console.log("Biplab");
+
+
+// Data Types:-
+       // Primitive Datatypes
+// number =>  2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value  (not undefined  its empty)    (output :- type Object)
+// undefined => not assigned any value
+// symbol => to find unique
+
+     // Non-Primitive Datatypes
+// objects
+// for check :- console.log(typeof varName)
