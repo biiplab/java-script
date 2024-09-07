7cb6fc6bf710cf5ac33794264ea4767e652794c2
biiplab <biiplabroyy@gmail.com>
2024-09-07 22:57:10 +0530

    update datatypes-summary file

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -24,7 +24,7 @@ console.log(id === anotherId);
 
 const heros = ["shaktiman", "naagraj", "doga"];
 let myObj = {
-    name: "hitesh",
+    name: "biplab",
     age: 22,
 }
 
@@ -32,6 +32,52 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap ( Non- Primitive)
+// Stack --- copy of a variable (not original val)
+// heap --- reference ( Original value will change)
+
+
+
+
+// Stack example that original does not change
+let myName = "biplab"
+
+let anotherName = myName
+anotherName = "biplab roy"
+
+
+// console.log(myName);
+// console.log(anotherName);
+
+
+
+
+// Heap example the Original value changes
+
+let userOne = {
+    email : "userOne@gmail.com",
+    upi : "user@oksbi"
+}
+let userTwo = userOne
+
+userTwo.email = "userTwo@gmail.com"
+// userTwo = {
+//     email : "newemail@gmail.com",
+//     upi : "new@okasis"
+// }
+
+console.log(userOne.email);      // Original value changed
+console.log(userTwo.email);   
+
+
+
+// Stack -- we get the Copy , so it will not change 
+// Heap -- we get the Original value, so it will change
