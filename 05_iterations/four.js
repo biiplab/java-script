3644e548eb25533d287a84ddb3e72221284fd94a
biiplab <biiplabroyy@gmail.com>
2024-09-13 01:58:48 +0530

    loops

---
+++ 05_iterations/four.js
@@ -0,0 +1,19 @@
+const myObj = {
+  js : "JavaScript",
+  cpp : "C++",
+  rb : "ruby",
+  swift : "swift by apple"
+}
+
+for (const key in myObj) {
+//  console.log(`${key} shortcut is for ${myObj[key]}`);
+}
+
+
+
+const programming = ["js", "rb", "py", "java"]
+
+for (const key in programming) {
+ console.log(programming[key]);
+ 
+}
